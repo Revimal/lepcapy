4ce912778c1127383c62069b998e65e7197b0f90
Revimal <seohho@gmail.com>
2016-10-07 15:53:05 +0900

    C99 Features Fix

--- lepcapy/src/pcap_queue.c
+++ lepcapy/src/pcap_queue.c
@@ -13,10 +13,12 @@ struct queue_list_s queue_list = {
 };
 
 int queue_init(){
+    int i;
+
     if(pthread_spin_init(get_queue_spinlock(), PTHREAD_PROCESS_PRIVATE))
         return -ETHREAD;
 
-    for(int i = 0; i < MAX_QUEUE_SIZE; ++i)
+    for(i = 0; i < MAX_QUEUE_SIZE; ++i)
         queue_elem(i).pcaprec_buf = NULL;
 
     return SUCCESS;
